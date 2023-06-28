384797960bffbb586bab64f32c1bd18881a63c48
roynaldi19 <roynaldi19@gmail.com>
2023-06-28 17:22:08 +0700

    update 23

--- app/src/main/java/com/roynaldi19/dc2_04viewandviewgroup/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dc2_04viewandviewgroup/MainActivity.kt
@@ -2,11 +2,15 @@ package com.roynaldi19.dc2_04viewandviewgroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roynaldi19.dc2_04viewandviewgroup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.title = "Google Pixel"
     }
 }
